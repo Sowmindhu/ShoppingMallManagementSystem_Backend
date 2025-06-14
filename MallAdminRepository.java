eb5eb5416199eb402233b5d0d60744b7a3a2f589
Sowmindhu <112560899+Sowmindhu@users.noreply.github.com>
2025-06-14 16:06:34 +0530

    Add files via upload

---
+++ MallAdminRepository.java
@@ -0,0 +1,7 @@
+package service.admin.mall;
+import org.springframework.data.jpa.repository.JpaRepository;  
+
+
+public interface MallAdminRepository extends JpaRepository<MallAdmin, Integer>{
+
+}
